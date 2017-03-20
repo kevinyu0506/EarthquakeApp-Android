29a3733f0c519ab7f43491b2daf45c032d5de454
kevinyu0506 <kevinyu05062006@gmail.com>
2017-03-20 20:42:04 +0800

    set settings page to the main page & complete fullscreen it

--- app/src/main/java/com/example/apple/earthquakeapp/settings_eqinfo.java
+++ app/src/main/java/com/example/apple/earthquakeapp/settings_eqinfo.java
@@ -5,7 +5,7 @@ import android.os.Bundle;
 import android.view.Window;
 import android.view.WindowManager;
 
-public class MainActivity extends AppCompatActivity {
+public class settings_eqinfo extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +15,6 @@ public class MainActivity extends AppCompatActivity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_settings_eqinfo);
     }
 }
