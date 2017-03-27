e2660e4d6f3f3c26caa2fec15493c65707d7a5cb
adrienne17l <adrienne0215@gmail.com>
2017-03-27 21:03:26 +0800

    map temp

--- app/src/main/java/tw/edu/bpmlab/mis/nccu/earthquakeapp/MapsActivity.java
+++ app/src/main/java/tw/edu/bpmlab/mis/nccu/earthquakeapp/MapsActivity.java
@@ -1,10 +1,14 @@
 package tw.edu.bpmlab.mis.nccu.earthquakeapp;
 
 import android.content.Intent;
+import android.location.Address;
+import android.location.Geocoder;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -15,13 +19,22 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MapStyleOptions;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import java.io.IOException;
+import java.util.List;
+
+import static tw.edu.bpmlab.mis.nccu.earthquakeapp.R.id.editText;
+
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
         final ImageButton alert = (ImageButton) findViewById(R.id.alarmButton);
         final ImageButton settings = (ImageButton) findViewById(R.id.settingButton);
+
         alert.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -49,7 +62,11 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+        
     }
+
+
+
 
     @Override
     public void onMapReady(GoogleMap map) {
