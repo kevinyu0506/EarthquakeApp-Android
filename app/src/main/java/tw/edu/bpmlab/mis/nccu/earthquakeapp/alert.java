f4e602e44fca70075a2a7ae83f66c085f02dedc4
xu3gl4jo <xu3gl4jo@gmail.com>
2017-04-10 21:56:20 +0800

    抓地址

--- app/src/main/java/tw/edu/bpmlab/mis/nccu/earthquakeapp/alert.java
+++ app/src/main/java/tw/edu/bpmlab/mis/nccu/earthquakeapp/alert.java
@@ -2,6 +2,8 @@ package tw.edu.bpmlab.mis.nccu.earthquakeapp;
 
 import android.content.Intent;
 import android.graphics.Color;
+import android.location.Location;
+import android.location.LocationListener;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -13,6 +15,12 @@ import android.view.View;
 import android.widget.ImageButton;
 
 //db
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.sql.ResultSet;
 import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
@@ -31,7 +39,7 @@ import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 
-public class alert extends AppCompatActivity implements SensorEventListener {
+public class alert extends AppCompatActivity implements SensorEventListener, LocationListener {
 
     protected TextView Date;
     protected int thisYear;
@@ -47,13 +55,18 @@ public class alert extends AppCompatActivity implements SensorEventListener {
     protected int setCountDownTime;
     protected long timeUntilFinish;
 
-    private SensorManager aSensorManager;
-    private Sensor aSensor;
-    private double gravity[] = new double[3];
+    public SensorManager aSensorManager;
+    public Sensor aSensor;
+    public double gravity[] = new double[3];
 //    private double eqGal;
 
     public TextView level;
     public TextView levelDescribe;
+
+    public long latitude;
+    public long longtitude;
+    public TextView location;
+    public String address;
 
 
 
@@ -96,6 +109,8 @@ public class alert extends AppCompatActivity implements SensorEventListener {
         setCountDownBar();
         sensor();
         getMagnitude();
+        findLocation(23.222,121.3232);
+        location.setText("123");
 
 
     }
@@ -160,22 +175,28 @@ public class alert extends AppCompatActivity implements SensorEventListener {
         countDown = (TextView) findViewById(R.id.countDown);
 
         setCountDownTime = (int) (Math.random() * 10 + 1) * 10000;
+//        setCountDownTime = 9000;
+
         new CountDownTimer(setCountDownTime, 1000) {
 
             @Override
             public void onFinish() {
-                countDown.setText("00:00");
+//                countDown.setText("00:00");
             }
 
             @Override
             public void onTick(long millisUntilFinished) {
 
-                timeUntilFinish = millisUntilFinished;
+//                timeUntilFinish = millisUntilFinished;
 
                 if (millisUntilFinished / 1000 % 60 >= 10) {
                     countDown.setText("0" + String.valueOf(millisUntilFinished / 60000) + ":" + String.valueOf(millisUntilFinished / 1000 % 60));
                 } else {
                     countDown.setText("0" + String.valueOf(millisUntilFinished / 60000) + ":0" + String.valueOf(millisUntilFinished / 1000 % 60));
+                }
+                if(millisUntilFinished == 0){
+                    countDown.setText("00:00");
+
                 }
 
             }
@@ -200,9 +221,11 @@ public class alert extends AppCompatActivity implements SensorEventListener {
                 while (progress > 0) {
                     try {
                         countDownBar.setProgress(progress);
-                        sleep(100);
+                        sleep((setCountDownTime / 100));
 //                        progress = progress - 5;
-                        progress = progress - (100 / (setCountDownTime / 100));
+//                        progress = progress - (100 / (setCountDownTime / 100));
+                        progress = progress - 1;
+
 //                        Log.d("1", Integer.toString(progress));
                     } catch (InterruptedException e) {
                         // TODO Auto-generated catch block
@@ -240,6 +263,7 @@ public class alert extends AppCompatActivity implements SensorEventListener {
 
 
     static connectDataBase connectDataBase = new connectDataBase();
+
     public void getMagnitude() {
 
         level = (TextView) findViewById(R.id.level);
@@ -256,17 +280,17 @@ public class alert extends AppCompatActivity implements SensorEventListener {
             while (rs.next()) {
                 level.setText(rs.getString("accelerationz"));
                 System.out.print(rs.getFloat("accelerationz"));
-                if(rs.getFloat("accelerationz") <= 2){
+                if (rs.getFloat("accelerationz") <= 2) {
                     levelDescribe.setText("LIGHT");
                     levelDescribe.setTextColor(Color.rgb(92, 201, 255));
                     level.setTextColor(Color.rgb(92, 201, 255));
 
-                }else if(rs.getFloat("accelerationz") >= 3 && rs.getFloat("accelerationz") <= 4){
+                } else if (rs.getFloat("accelerationz") >= 3 && rs.getFloat("accelerationz") <= 4) {
                     levelDescribe.setText("MEDIUM");
                     levelDescribe.setTextColor(Color.rgb(255, 209, 5));
                     level.setTextColor(Color.rgb(255, 209, 5));
 
-                }else if(rs.getFloat("accelerationz") >= 5){
+                } else if (rs.getFloat("accelerationz") >= 5) {
                     levelDescribe.setText("SEVERE");
                     levelDescribe.setTextColor(Color.rgb(235, 61, 125));
                     level.setTextColor(Color.rgb(235, 61, 125));
@@ -278,6 +302,78 @@ public class alert extends AppCompatActivity implements SensorEventListener {
     }
 
 
+    public String[] findLocation(double lon, double lat) {
+
+        location = (TextView)findViewById(R.id.location) ;
+
+        String locationData[] = new String[3];
+        try {
+            String http = "http://maps.googleapis.com/maps/api/geocode/json?latlng=" + lat + "," + lon + "&language=zh-TW&sensor=true";
+            URL url = new URL(http);
+            HttpURLConnection huc = (HttpURLConnection)url.openConnection();
+            BufferedReader br = new BufferedReader(new InputStreamReader(huc.getInputStream(),"UTF-8"));
+            String str = "";
+            StringBuffer sb = new StringBuffer();
+
+            while (null != ((str=br.readLine()))){
+                sb.append(str);
+                if(str.contains("formatted_address")){
+                    str = str.replace("formatted_address","");
+                    str = str.replace(" ","");
+                    str = str.replace(":","");
+                    str = str.replace("Unnamed Road","");
+                    str = str.replace(",","");
+                    locationData[0] = str;
+                }else if(str.contains("coordinates")){
+                    str = str.replace("coordinates","");
+                    str = str.replace("Point","");
+                    str = str.replace(" ","");
+                    str = str.replace(">","");
+                    str = str.replace("<","");
+                    str = str.replace("/","");
+                    String tmp[] = str.split(",");
+                    locationData[1] = tmp[0];
+                    locationData[2] = tmp[1];
+                }
+            }
+            br.close();
+            String xmlResponse = sb.toString();
+            huc.disconnect();
 
 
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        address = locationData[0];
+        return locationData;
+
+    }
+
+
+    @Override
+    public void onLocationChanged(Location loc) {
+        if(loc != null){
+            String add[] = findLocation(loc.getLongitude(), loc.getLatitude());
+//            location.setText(add[0]);
+
+        }
+
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+
+    }
 }
