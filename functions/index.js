1d0abe9c0fbfe62863be537687352a2fd3107fdc
kevinyu0506 <kevinyu05062006@gmail.com>
2017-04-28 17:59:50 +0800

    成功的一半

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,6 @@
 var functions = require('firebase-functions');
+var admin = require('firebase-admin');
+admin.initializeApp(functions.config().firebase);
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -6,3 +8,19 @@ var functions = require('firebase-functions');
 exports.helloWorld = functions.https.onRequest((request, response) => {
  response.send("Hello from Firebase!");
 });
+
+
+exports.uploadEqCenter = functions.database.ref('/eqData/{pushId}')
+    .onWrite(event => {
+
+    	var collectionRef = event.data.ref.parent;
+ 		var eqCenterRef = collectionRef.parent.child('eqCenter');
+ 		var eventSnapshot = event.data;
+
+        var localLatitude = eventSnapshot.child("latitude").val();
+        var localLongitude = eventSnapshot.child("longitude").val();
+        var localMagnitude = eventSnapshot.child("magnitude").val();
+
+	admin.database().ref('eqCenter').update({longitude:localLongitude, latitude:localLatitude, magnitude:87});
+});
+
