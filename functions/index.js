9c48e8f8f0262b57dd93a9c97b84b32ec42b6f25
kevinyu0506 <kevinyu05062006@gmail.com>
2017-04-30 21:21:34 +0800

    (0~10,0~10)

--- functions/index.js
+++ functions/index.js
@@ -6,42 +6,69 @@ admin.initializeApp(functions.config().firebase);
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
 exports.helloWorld = functions.https.onRequest((request, response) => {
- response.send("Hello from Firebase!");
+    response.send("Hello from Firebase!");
 });
 
 
 exports.uploadEqCenter = functions.database.ref('/eqData/{pushId}')
     .onWrite(event => {
 
-    	var collectionRef = event.data.ref.parent;
- 		var eqCenterRef = collectionRef.parent.child('eqCenter');
- 		var eventSnapshot = event.data;
+        var collectionRef = event.data.ref.parent;
+        var eqCenterRef = collectionRef.parent.child('eqCenter');
+        var eventSnapshot = event.data;
 
         var localLatitude = eventSnapshot.child("latitude").val();
         var localLongitude = eventSnapshot.child("longitude").val();
         var localMagnitude = eventSnapshot.child("magnitude").val();
+        var localTime = eventSnapshot.child("time").val();
 
 
-        var geocoder = new google.maps.Geocoder();
-		// google.maps.LatLng 物件
-		var coord = new google.maps.LatLng(23.5, 121);
+        // var geocoder = new google.maps.Geocoder();
+        // // google.maps.LatLng 物件
+        // var coord = new google.maps.LatLng(23.5, 121);
 
-		// // 傳入 latLng 資訊至 geocoder.geocode
-		// geocoder.geocode({'latLng': coord }, function(results, status) {
-		//   if (status === google.maps.GeocoderStatus.OK) {
- 	// 	   // 如果有資料就會回傳
- 	// 	   if (results) {
-  // 		    address = results[4].formatted_address;
-  // 		    admin.database().ref('eqCenter').update({address : address});
-  // 		    }
-  // 		}
-  // 		// 經緯度資訊錯誤
- 	// 		 else {
- 	// 	   alert("Reverse Geocoding failed because: " + status);
- 	// 			 }
-		// });
+        // // 傳入 latLng 資訊至 geocoder.geocode
+        // geocoder.geocode({'latLng': coord }, function(results, status) {
+        //   if (status === google.maps.GeocoderStatus.OK) {
+        //     // 如果有資料就會回傳
+        //     if (results) {
+        //        address = results[4].formatted_address;
+        //        admin.database().ref('eqCenter').update({address : address});
+        //        }
+        //    }
+        //    // 經緯度資訊錯誤
+        //     else {
+        //     alert("Reverse Geocoding failed because: " + status);
+        //       }
+        // });
 
 
-	admin.database().ref('eqCenter').update({longitude:78, latitude:88, magnitude:3, address:"台灣台北市"});
-});
+
+
+        //(0~10,0~10)(11~20,11~20)(21~30,21~30)(31~40,31~40)
+        //  0    0      1     1      2     2      3     3
+
+        var localX = Math.floor(localLongitude/10);
+        var localY = Math.floor(localLatitude/10);
+
+
+        admin.database().ref('eqCenter').update({ longitude: 78, latitude: 88, magnitude: 3, address: "台灣台北市" });
+        // admin.database().ref('eqData('+localX+','+localY+')').push().set({longitude: localLongitude, latitude: localLatitude, magnitude: localMagnitude, time: localTime});
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
