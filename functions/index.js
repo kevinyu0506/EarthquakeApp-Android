d6630d4aae7bc92d631ea2020b23e5ada3682272
kevinyu0506 <kevinyu05062006@gmail.com>
2017-05-04 22:28:35 +0800

    npm install geocoder

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,6 @@
 var functions = require('firebase-functions');
 var admin = require('firebase-admin');
+var geocoder = require('geocoder');
 admin.initializeApp(functions.config().firebase);
 
 // // Create and Deploy Your First Cloud Functions
@@ -25,24 +26,15 @@ exports.uploadEqCenter = functions.database.ref('/eqData/{pushId}')
         var eqDataID = eventSnapshot.child("eqDataID").val();
 
 
-        // var geocoder = new google.maps.Geocoder();
-        // // google.maps.LatLng 物件
-        // var coord = new google.maps.LatLng(23.5, 121);
 
-        // // 傳入 latLng 資訊至 geocoder.geocode
-        // geocoder.geocode({'latLng': coord }, function(results, status) {
-        //   if (status === google.maps.GeocoderStatus.OK) {
-        //     // 如果有資料就會回傳
-        //     if (results) {
-        //        address = results[4].formatted_address;
-        //        admin.database().ref('eqCenter').update({address : address});
-        //        }
-        //    }
-        //    // 經緯度資訊錯誤
-        //     else {
-        //     alert("Reverse Geocoding failed because: " + status);
-        //       }
-        // });
+        
+        geocoder.reverseGeocode(33.7489, -84.3789, function(err, data) {
+            // do something with data 
+            //show all the data
+            // console.log(data);
+            //show the target information
+            console.log(data.results[4].formatted_address)
+        });
 
 
 
